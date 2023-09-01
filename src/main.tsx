76fba0b5cb46cc97bf148798acca3d416cc36733
Sanka <sankasanjeevadg@gmail.com>
2023-09-01 18:31:33 +0530

    Configured main routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,53 @@
-import React from 'react';
+import { ReactNode, StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom';
 import App from './App';
 import './index.css';
 
+function ProtectedRoute({ children }: { children: ReactNode }) {
+  const token = 'useAuth();';
+
+  if (!token) {
+    return children;
+  }
+  return <Navigate to="/assets-manager/login" />;
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/assets-manager',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: (
+          <ProtectedRoute>
+            <Navigate to="dashboard" />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: 'login',
+        element: <div>login</div>,
+      },
+      {
+        path: 'dashboard',
+        element: (
+          <ProtectedRoute>
+            <div>Dashboard</div>
+          </ProtectedRoute>
+        ),
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
 );
